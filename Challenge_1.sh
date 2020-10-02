7423a0abbeac821a2772e13e05732eb6262af482
R4ZZ-berry <ehernandezjr@pm.me>
2020-10-02 16:07:53 -0500

    Answer to challenge 1

--- Challenge_1.sh
+++ Challenge_1.sh
@@ -6,7 +6,7 @@
 #Select a currently active network interface. Identify the IP range of the network. Ping all IPs in the range and print only IPs that respond.
 
 #How to get desired results of challenge.
-#Scan all interfaces and identify which are active and omit any sudo NICs. Once the interface has been identified, obtain the network IP and CIDR (subnet). Next, scan the network with the appropriate CIDR utilizing Nmap and filter out the network and broadcast IPs. Print the filtered out IPs into Text file. Use the text file to ping against all the listed IPs and display only IPs responding.
+#Scan all interfaces and identify which are active and omit any sudo NICs. Once the interface has been identified, obtain the network IP and CIDR (subnet). Next, generate a list of IPs in the subnet using nmap -sL. Print the filtered out IPs into Text file. Use the text file to ping against all the listed IPs and display only IPs responding.
 
 
 # Identify all active physical interfaces and iterate through them. (E.g. the system may be connected to an ethernet and wireless interface with IPs on each.)
@@ -27,4 +27,17 @@ for i in $(ip addr | awk '/state UP/ {print $2}' | awk -F: '$0 !~ "lo|vmnet"{pri
         do
         ping -c 1 -W 0.1 $j | grep "bytes from" |cut -f4 -d" " | sed 's/.$//'
     done
-
+#
+#Test results from home lab
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+#razz@Muninn:~$ bash test1
+#List of IPs responding on interface enp0s31f
+#192.168.1.236
+#192.168.1.243
+#192.168.1.251
+#192.168.1.253
+#192.168.1.254
+#List of IPs responding on interface wlp0s20f
+#172.20.10.1
+#172.20.10.3
+#172.20.10.4
